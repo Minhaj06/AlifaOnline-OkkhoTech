10b4123a42b2d10ac09b4cab4758e50b44327805
Minhaj06 <milonmdminhajkobir@gmail.com>
2022-12-28 22:03:30 +0600

    Working On Home Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,42 @@ $(".filter-menu").on("click", "li", function () {
   var filterValue = $(this).attr("data-filter");
   $grid.isotope({ filter: filterValue });
 });
+
+// Initialize Swiper
+const swiper = new Swiper(".swiper", {
+  // slidesPerView: 4,
+  loop: true,
+  spaceBetween: 20,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+  // direction: getDirection(),
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // on: {
+  //   resize: function () {
+  //     swiper.changeDirection(getDirection());
+  //   },
+  // },
+});
+
+// function getDirection() {
+//   const windowWidth = window.innerWidth;
+//   const direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+//   return direction;
+// }

64bddb7adecaf1b8e8921d685e0490a04694742f
Minhaj06 <milonmdminhajkobir@gmail.com>
2022-12-31 16:41:14 +0600

    Working On Shop Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,3 +101,20 @@ const testimonialSpeechSwiper = new Swiper(".testimonial-speech-swiper", {
     clickable: true,
   },
 });
+
+// Price Range Slider
+let rangeSlider = document.querySelector(".range-slider");
+let rangeSliderAfter = window.getComputedStyle(rangeSlider, "::after");
+
+let rangeSelector = document.querySelector(".range-selector");
+let rangeSelectedValue = document.querySelector(".range-selected-value");
+
+rangeSelectedValue.innerHTML = rangeSlider.value * 100 + "৳";
+
+rangeSlider.oninput = function () {
+  rangeSelector.style.left = this.value + "%";
+
+  rangeSelectedValue.innerHTML = this.value * 100 + "৳";
+
+  rangeSlider.style.setProperty("--rangeSliderWidth", this.value + "%");
+};
